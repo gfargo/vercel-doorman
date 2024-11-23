ea0b68a063a8fd04d466f5d9d0fd98f92bbca1f4
Griffen Fargo <ghfargo@gmail.com>
2024-11-23 09:17:22 -0500

    chore(jest): add chalk mock

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,7 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   modulePathIgnorePatterns: ['<rootDir>/dist/'],
+  moduleNameMapper: {
+    '^chalk$': '<rootDir>/src/tests/__mocks__/chalk.ts',
+  },
 }
