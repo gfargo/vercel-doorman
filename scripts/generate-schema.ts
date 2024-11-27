ffa881bcee83495ac61dd23e35cd1af444e41e2c
Griffen Fargo <ghfargo@gmail.com>
2024-11-27 12:39:33 -0500

    build: update project configs and improve schema generation output

--- scripts/generate-schema.ts
+++ scripts/generate-schema.ts
@@ -16,15 +16,16 @@ const schema = createGenerator(config).createSchema(config.type)
 // Add some metadata to the schema
 const schemaWithMeta = {
   $schema: 'http://json-schema.org/draft-07/schema#',
-  title: 'Vercel Doorman Firewall Configuration',
-  description: 'Schema for vercel-doorman firewall configuration files',
+  title: 'Doorman Config',
+  description: 'Schema for vercel-doorman project configuration files',
   ...schema,
 }
 
+// Write the schema to a file as a variable so it can be used for validation.
 const schemaAsVariable = `export const schema = ${JSON.stringify(schemaWithMeta, null, 2)}`
-
-writeFileSync(schemaOutputPath, JSON.stringify(schemaWithMeta, null, 2))
 writeFileSync(schemaAsVariableOutputPath, schemaAsVariable)
 
+writeFileSync(schemaOutputPath, JSON.stringify(schemaWithMeta, null, 2))
+
 // eslint-disable-next-line no-console
-console.log(`Schema generated at ${schemaOutputPath}`)
+console.log(`\x1b[32m✨ Schema generated at \x1b[1m${schemaOutputPath}\x1b[0m\x1b[32m\x1b[0m`)
